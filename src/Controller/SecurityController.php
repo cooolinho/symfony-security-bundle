63f5737721c3c26dd852d4ddc934765f91727af2
cooolinho <coding@cooolinho.de>
2021-07-18 17:37:21 +0200

    move security controller

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Cooolinho\Bundle\SecurityBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class SecurityController extends AbstractSecurityController
+{
+    public const ROUTE_LOGIN = 'app_login';
+    public const ROUTE_LOGOUT = 'app_logout';
+
+    /**
+     * @Route("/login", name="app_login")
+     * @param AuthenticationUtils $authenticationUtils
+     * @return Response
+     */
+    public function login(AuthenticationUtils $authenticationUtils): Response
+    {
+        return parent::login($authenticationUtils);
+    }
+
+    /**
+     * @Route("/logout", name="app_logout")
+     */
+    public function logout(): void
+    {
+        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+    }
+}
